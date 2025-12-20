f960559f7afaa9d00476a68aeb2541ee0e189e97
keshav-maheshwari707 <keshav.maheshwari707@gmail.com>
2025-12-20 12:38:56 +0530

    bubble sort again

--- sorting/bubbleSort.cpp
+++ sorting/bubbleSort.cpp
@@ -1,9 +1,9 @@
 #include<iostream>
 using namespace std;
-void bubbleSort(int arr[], int n)
+void bubbleSort(int arr[], int size)
 {
-    for(int i = 0; i < n-1; i++) {
-        for(int j = 0; j < n-i-1; j++) {
+    for(int i = 0; i < size-1; i++) {
+        for(int j = 0; j < size-i-1; j++) {
             if(arr[j] > arr[j+1]) {
                 swap(arr[j], arr[j+1]);
             }
@@ -11,9 +11,9 @@ void bubbleSort(int arr[], int n)
     }
 }
 
-void printArray(int arr[], int n) 
+void printArray(int arr[], int size) 
 {
-    for(int i = 0; i < n; i++) {
+    for(int i = 0; i < size; i++) {
         cout << arr[i] << " ";
     }
     cout << endl;
@@ -21,9 +21,9 @@ void printArray(int arr[], int n)
 
 int main()
 {
-    int n = 5;
     int arr[] = {4, 1, 5, 2, 3};
-    bubbleSort(arr, n);
-    printArray(arr, n);
+    int size = sizeof(arr)/sizeof(arr[0]);
+    bubbleSort(arr, size);
+    printArray(arr, size);
     return 0;
 }
